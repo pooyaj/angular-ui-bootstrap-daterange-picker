292c4c4ae646486c224fcd0c6bb81bd39dbf39fa
Pooya Jaferian <pooya.j@gmail.com>
2016-02-09 16:05:20 -0800

    added a basic date range functionality and cleaning the boilerplate code

--- app.js
+++ app.js
@@ -1,84 +1,39 @@
 angular.module('ui.bootstrap.demo', ['ngAnimate', 'ui.bootstrap']);
 angular.module('ui.bootstrap.demo').controller('DatepickerDemoCtrl', function ($scope) {
-  $scope.today = function() {
-    $scope.dt = new Date();
+
+  $scope.dt = new Date();
+  
+  $scope.dateRange = {
+      end: undefined,
+      start: undefined 
   };
-  $scope.today();
-
-  $scope.clear = function() {
-    $scope.dt = null;
-  };
-
-  // Disable weekend selection
-  $scope.disabled = function(date, mode) {
-    return mode === 'day' && (date.getDay() === 0 || date.getDay() === 6);
-  };
-
-  $scope.toggleMin = function() {
-    $scope.minDate = $scope.minDate ? null : new Date();
-  };
-
-  $scope.toggleMin();
-  $scope.maxDate = new Date(2020, 5, 22);
-
-  $scope.open1 = function() {
-    $scope.popup1.opened = true;
-  };
-
-  $scope.open2 = function() {
-    $scope.popup2.opened = true;
-  };
-
-  $scope.setDate = function(year, month, day) {
-    $scope.dt = new Date(year, month, day);
-  };
-
-  $scope.dateOptions = {
-    formatYear: 'yy',
-    startingDay: 1
-  };
-
-  $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
-  $scope.format = $scope.formats[0];
-  $scope.altInputFormats = ['M!/d!/yyyy'];
-
-  $scope.popup1 = {
-    opened: false
-  };
-
-  $scope.popup2 = {
-    opened: false
-  };
-
-  var tomorrow = new Date();
-  tomorrow.setDate(tomorrow.getDate() + 1);
-  var afterTomorrow = new Date();
-  afterTomorrow.setDate(tomorrow.getDate() + 1);
-  $scope.events =
-    [
-      {
-        date: tomorrow,
-        status: 'full'
-      },
-      {
-        date: afterTomorrow,
-        status: 'partially'
-      }
-    ];
-
+  
+  $scope.$watch('dt',function(){    
+    if(!$scope.dt){
+        return;
+    }
+    
+    if(!$scope.dateRange.start || $scope.dateRange.start > $scope.dt){
+        $scope.dateRange.start = $scope.dt;         
+    } else {
+        $scope.dateRange.end = $scope.dt;
+    }
+            
+    $scope.dt=null;            
+  });    
+    
   $scope.getDayClass = function(date, mode) {
     if (mode === 'day') {
       var dayToCheck = new Date(date).setHours(0,0,0,0);
-
-      for (var i = 0; i < $scope.events.length; i++) {
-        var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
-
-        if (dayToCheck === currentDay) {
-          return $scope.events[i].status;
-        }
-      }
+      var start = new Date($scope.dateRange.start).setHours(0,0,0,0);
+      var end = new Date($scope.dateRange.end).setHours(0,0,0,0);
+      if (dayToCheck === start || dayToCheck === end) {
+          return 'full';
+      } else if (dayToCheck > start && dayToCheck < end) {
+          return 'partially';
+      };
     }
-
     return '';
   };
+  
 });
